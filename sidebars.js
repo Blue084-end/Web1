56a2b883e328ac3e74fc35742e054347b27763a7
Blue084-end <Azend11110000@gmail.com>
2025-10-24 10:29:59 +0700

    Tích hợp Decap CMS và sidebar nhiều tab

--- sidebars.js
+++ sidebars.js
@@ -1,9 +1,16 @@
+
+
 module.exports = {
-  tutorialSidebar: [
+  docs: [
     {
       type: 'category',
-      label: 'Tài liệu',
-      items: ['intro', 'huong-dan'],
+      label: '🌱 Tài liệu cá nhân',
+      items: ['gioi-thieu', 'ghi-chu-ngay'],
+    },
+    {
+      type: 'category',
+      label: '📂 Tài liệu upload',
+      items: ['tai-lieu-1', 'tai-lieu-2'],
     },
   ],
 };
